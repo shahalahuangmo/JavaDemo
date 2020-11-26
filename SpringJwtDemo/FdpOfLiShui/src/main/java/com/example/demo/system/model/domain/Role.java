62e35016bb1ac384988d8e7de4c0020730770283
pengnanfa <pengnanfa@126.com>
2020-11-26 19:25:53 +0800

    22

--- SpringJwtDemo/FdpOfLiShui/src/main/java/com/example/demo/system/model/domain/Role.java
+++ SpringJwtDemo/FdpOfLiShui/src/main/java/com/example/demo/system/model/domain/Role.java
@@ -3,6 +3,7 @@ package com.example.demo.system.model.domain;
 import com.example.demo.system.common.BaseOrderEntity;
 import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
 import com.gitee.sunchenbin.mybatis.actable.annotation.IsNotNull;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Table;
 import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
 import lombok.Data;
 
@@ -10,7 +11,8 @@ import lombok.Data;
  * 系统角色表
  */
 @Data
-public class Role  extends BaseOrderEntity
+@Table(name = "role")
+public class Role extends BaseOrderEntity
 {
     /**
      * 角色名称
@@ -21,12 +23,12 @@ public class Role  extends BaseOrderEntity
     /**
      * 角色名称
      */
-    @Column(name = "accessName",type = MySqlTypeConstant.VARCHAR,length = 255)
+    @Column(name = "roleCode",type = MySqlTypeConstant.VARCHAR,length = 255)
     @IsNotNull
     private String roleCode;
     /**
      * 描述
      */
-    @Column(name = "accessName",type = MySqlTypeConstant.VARCHAR,length = 2000)
+    @Column(name = "displayName",type = MySqlTypeConstant.VARCHAR,length = 2000)
     private String displayName;
 }
