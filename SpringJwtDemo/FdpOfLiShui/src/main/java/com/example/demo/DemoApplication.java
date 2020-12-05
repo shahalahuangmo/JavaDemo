6e23102dc2076e8465e24e98617565259a5dca4e
pengnanfa <pengnanfa@126.com>
2020-12-05 23:43:37 +0800

    11

--- SpringJwtDemo/FdpOfLiShui/src/main/java/com/example/demo/DemoApplication.java
+++ SpringJwtDemo/FdpOfLiShui/src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //这里需要使用 tk.mybatis.spring.annotation.MapperScan 替换 org.mybatis.spring.annotation.MapperScan
@@ -11,8 +13,15 @@ import org.mybatis.spring.annotation.MapperScan;
 @MapperScan("com.example.demo.system.mapper")
 public class DemoApplication {
 
+    static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+        logger.debug("taiyonghai debug");
+        logger.info("taiyonghai info");
+        logger.error("taiyonghai error");
+        logger.warn("taiyonghai warn");
+
     }
 
 }
